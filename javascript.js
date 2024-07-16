1c664b8a9db55536cddcf3fdea8edce5c08203c7
SABARI G <143690545+sabarig08@users.noreply.github.com>
2024-07-16 17:45:28 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// //problem 1 Array Manipulation (add and remove)
+let fruits=["Apple","orange","mango"];
+console.log(fruits);
+fruits.push("pinaapple");
+console.log(fruits);
+fruits.shift("Apple");
+console.log(fruits);
+
+
+//problem 2 Array creation and Intialization
+let form=["fname","lname","phone no","Email","Address"];
+console.log(form);
+
+
+//problem 3  Sorting  and reverse
+const user=["name","email","address"];
+user.sort();
+console.log(user);
+const user1=["11","8","5","1"];
+console.log(user1.reverse());
+
+
+
+//problem 4 Array Mapping
+const value=[100,4,16,4,36];
+const val=value.map(Math.sqrt);
+console.log(val);
+
+
+//problem 5 Array indexof()
+let text=["a","b","c","d","e"];
+console.log(text.indexOf("c"));
